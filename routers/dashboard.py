d7c4150fcd4f6757b6f4e0ebeac1c51362593f30
abdoulacherif <abdoula13cherif@gmail.com>
2026-08-28 21:32:44 +0100

    Delete dashboard.py

--- routers/dashboard.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import FileResponse
-
-router = APIRouter(prefix="/dashboard", tags=["dashboard"])
-
-@router.get("/")
-async def dashboard():
-    return FileResponse("templates/dashboard.html")
