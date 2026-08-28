3fd734536cb9eb1ecf92d484fc23838ac6996b77
abdoulacherif <abdoula13cherif@gmail.com>
2026-08-28 21:47:35 +0100

    Create kotlin/com/locafric/android/MainActivity.kt

---
+++ androidApp/src/main/kotlin/com/locafric/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.locafric.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // L'interface (écrans bailleur/locataire) viendra ici
+    }
+}
