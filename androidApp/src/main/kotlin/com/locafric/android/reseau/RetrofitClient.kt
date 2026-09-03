9548d68a5d384338185ca3e2afa0c3be1c8eaefc
abdoulacherif <abdoula13cherif@gmail.com>
2026-09-03 08:40:50 +0100

    Update RetrofitClient.kt

--- androidApp/src/main/kotlin/com/locafric/android/reseau/RetrofitClient.kt
+++ androidApp/src/main/kotlin/com/locafric/android/reseau/RetrofitClient.kt
@@ -1,15 +1,23 @@
 package com.locafric.android.reseau
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    // Adresse de ton backend déployé sur Vercel
     private const val URL_BASE = "https://locafric.vercel.app/api/"
+
+    private val clientHttp = OkHttpClient.Builder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
+        .build()
 
     val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(URL_BASE)
+            .client(clientHttp)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
